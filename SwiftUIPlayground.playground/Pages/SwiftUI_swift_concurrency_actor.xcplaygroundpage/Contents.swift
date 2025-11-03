fbbc8393b6b73256b753c1823171bbc68cf5854f
surajglokhande <surajglokhande@gmail.com>
2025-11-03 08:38:30 +0530

    - concurrency actor added.

---
+++ SwiftUIPlayground.playground/Pages/SwiftUI_swift_concurrency_actor.xcplaygroundpage/Contents.swift
@@ -0,0 +1,76 @@
+//: [Previous](@previous)
+
+import Foundation
+import SwiftUI
+import PlaygroundSupport
+
+class MyDataManager {
+    static let shared = MyDataManager()
+    
+    private init() {}
+    
+    var data: [String] = []
+    
+    func getRandomData() -> String? {
+        self.data.append(UUID().uuidString)
+        print(Thread.current)
+        return data.randomElement()
+    }
+}
+
+struct SwiftUI_swift_concurrency_actor: View {
+    var body: some View {
+        
+        TabView {
+            Tab("Home", systemImage: "house.fill", content: {
+                HomeView()
+            })
+            Tab("Profile", systemImage: "magnifyingglass", content: {
+                ProfileView()
+            })
+        }
+        
+    }
+}
+
+struct HomeView: View {
+    
+    let manager = MyDataManager.shared
+    @State private var text: String = ""
+    let timer = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
+    
+    var body: some View {
+        ZStack {
+            
+            Color.yellow.opacity(1)
+            Text("\(text)")
+            
+        }
+        .onReceive(timer) { _ in
+            DispatchQueue.global(qos: .background).async {
+                if let data = manager.getRandomData() {
+                    DispatchQueue.main.async {
+                        text = data
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct ProfileView: View {
+    var body: some View {
+        ZStack {
+            
+            Color.red.opacity(1)
+            Text("Profile")
+            
+        }
+    }
+}
+
+PlaygroundPage.current.setLiveView(
+    SwiftUI_swift_concurrency_actor()
+        .frame(width: 390, height: 800)
+)
+//: [Next](@next)
