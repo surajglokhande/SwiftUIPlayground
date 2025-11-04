12dbc478910db372da064a8704f7dacac3d82dbc
surajglokhande <surajglokhande@gmail.com>
2025-11-04 17:53:06 +0530

    - swift concurrency sendable file updated

--- SwiftUIPlayground.playground/Pages/SwiftUI_swift_concurrency_sendable.xcplaygroundpage/Contents.swift
+++ SwiftUIPlayground.playground/Pages/SwiftUI_swift_concurrency_sendable.xcplaygroundpage/Contents.swift
@@ -1,7 +1,61 @@
 //: [Previous](@previous)
+import SwiftUI
+import PlaygroundSupport
 
-import Foundation
+struct UserInfo: Sendable {
+    var name: String
+}
 
-var greeting = "Hello, playground"
+final class ClassUserInfo: @unchecked Sendable {
+    private var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    func getClassData(_ update: String) {
+        self.name = update
+    }
+}
+
+actor SendableUserInfo {
+    func getStructUser(_ info: UserInfo) {
+        //
+    }
+    
+    func getClassUser(_ info: ClassUserInfo) {
+        info.getClassData("new data")
+    }
+}
+
+@Observable
+class SendableViewModel {
+    
+    var manager = SendableUserInfo()
+    
+    func getData() async {
+        await manager.getStructUser(UserInfo(name: "Suraj"))
+        await manager.getClassUser(ClassUserInfo(name: "Suraj"))
+    }
+}
+
+
+
+struct SwiftUI_swift_concurrency_sendable: View {
+    
+    @State private var viewModel = SendableViewModel()
+    
+    var body: some View {
+        Text("Hello World!")
+            .task {
+                await self.viewModel.getData()
+            }
+    }
+}
+
+PlaygroundPage.current.setLiveView(
+    SwiftUI_swift_concurrency_sendable()
+        .frame(width: 390, height: 800)
+)
 
 //: [Next](@next)
