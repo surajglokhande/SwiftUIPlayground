4deb8f4545f2b47fddc4ba6b60e56f315121ecfe
surajglokhande <surajglokhande@zohomail.in>
2025-11-26 20:52:22 +0530

    - view builder updated.

--- SwiftUIPlayground.playground/Pages/SwiftUI_view_builder.xcplaygroundpage/Contents.swift
+++ SwiftUIPlayground.playground/Pages/SwiftUI_view_builder.xcplaygroundpage/Contents.swift
@@ -1,19 +1,31 @@
 //: [Previous](@previous)
 /*:
- # ViewBuilder
- ## A custom parameter attribute that constructs views from closures.
+ 
+ ## ResultBuilder
+ - The @resultBuilder is a Swift language feature that acts like a "mini-compiler" or a special transformer, allowing you to create custom, highly readable syntax within a function or closure.
+ - Transforms a Sequence into a Single Result: It takes a series of expressions (each line inside the closure) and combines them into a single, cohesive return value.
+ 
+ ## ViewBuilder
+ - A custom parameter attribute that constructs views from closures.
+ - You only need to explicitly write @ViewBuilder when creating custom view properties that accept multiple views:
  */
 import Foundation
 import SwiftUI
 import PlaygroundSupport
 struct SwiftUI_view_builder: View {
+    @State private var isLogin:Bool = false
     func createViewUsingViewBuilderTwo() -> some View {
-        Text("This is a View Builder by func using @ViewBuilder property Wrapper")
-            .multilineTextAlignment(.center)
+        if isLogin {
+            Text("This is a View Builder by func using @ViewBuilder property Wrapper")
+                .multilineTextAlignment(.center)
+        }else {
+            Text("This is a View Builder by func using @ViewBuilder property Wrapper")
+                .multilineTextAlignment(.center)
+        }
     }
     var body: some View {
         Spacer()
-        TopHeaderView(name: "header_view") {
+        TopHeaderView(/*name: "header_view"*/) {
             Text("This is struct View and return content by callback")
                 .multilineTextAlignment(.center)
         }
@@ -31,7 +43,7 @@ struct SwiftUI_view_builder: View {
 }
 
 struct TopHeaderView<Content: View>: View {
-    let name: String
+    //let name: String
     let content: () -> Content
 
     var body: some View {
@@ -42,7 +54,10 @@ struct TopHeaderView<Content: View>: View {
 }
 
 extension View {
+    @ViewBuilder //
     func createViewUsingViewBuilderOne() -> some View {
+        Text("This is a View Builder by func using extension")
+            .multilineTextAlignment(.center)
         Text("This is a View Builder by func using extension")
             .multilineTextAlignment(.center)
     }
